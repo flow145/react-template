a17a1a41b403048eede06f1651d9acf520be7524
flow145 <268885866+flow145@users.noreply.github.com>
2026-03-26 17:03:51 +0500

    feat: set up import aliases and add https for dev server

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import mkcert from 'vite-plugin-mkcert'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths(), mkcert()],
 })
