697df462034deb015732fc6ef6f290c9bfe221ef
flow145 <268885866+flow145@users.noreply.github.com>
2026-03-27 11:34:15 +0500

    chore: fix formatting and linting issues

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
+const container = document.getElementById('root')
+if (!container) throw new Error('No root element!')
+
+createRoot(container).render(
   <StrictMode>
     <App />
   </StrictMode>,
